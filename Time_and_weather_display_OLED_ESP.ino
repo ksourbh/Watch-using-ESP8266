9c5646002f8665f75f1bd1160928fff243cff8b2
Livid Design Inc <105784717+ksourbh@users.noreply.github.com>
2022-05-18 15:42:46 +0530

    Add files via upload
    Time Zones are "pool.ntp.org","time.nist.gov"

---
+++ Time_and_weather_display_OLED_ESP.ino
@@ -0,0 +1,139 @@
+/*********
+  Sourbh kumar
+  My Design and Electronics work is at www.grabcad.com/sourbh  
+*********/
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+const char* ssid = "Haunted";
+const char* password = "kumar@12";
+
+int ledPin = 13;
+
+int timezone = 7 * 3600;
+int dst = 0;
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+
+
+void setup() {
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  // Clear the buffer.
+  display.clearDisplay();
+  display.display();
+  
+  pinMode(ledPin,OUTPUT);
+  digitalWrite(ledPin,LOW);
+
+  Serial.begin(115200);
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  
+  display.setCursor(0,0);
+  display.println("Wifi connecting to ");
+  display.println( ssid );
+
+  WiFi.begin(ssid,password);
+ 
+  display.println("\nConnecting");
+
+  display.display();
+
+  while( WiFi.status() != WL_CONNECTED ){
+      delay(500);
+      display.print("."); 
+      display.display();       
+  }
+
+  // Clear the buffer.
+  display.clearDisplay();
+  display.display();
+  display.setCursor(0,0);
+  
+  display.println("Wifi Connected!");
+  display.print("IP:");
+  display.println(WiFi.localIP() );
+
+  display.display();
+
+  configTime(timezone, dst, "pool.ntp.org","time.nist.gov");
+  display.println("\nWaiting for NTP...");
+
+  while(!time(nullptr)){
+     Serial.print("*");
+     
+     delay(1000);
+  }
+  display.println("\nTime response....OK"); 
+  display.display();  
+  delay(1000);
+
+  display.clearDisplay();
+  display.display();
+}
+
+void loop() {
+  
+  time_t now = time(nullptr);
+  struct tm* p_tm = localtime(&now);
+  
+  Serial.print(p_tm->tm_mday);
+  Serial.print("/");
+  Serial.print(p_tm->tm_mon + 1);
+  Serial.print("/");
+  Serial.print(p_tm->tm_year + 1900);
+  
+  Serial.print(" ");
+  
+  Serial.print(p_tm->tm_hour);
+  Serial.print(":");
+  Serial.print(p_tm->tm_min);
+  Serial.print(":");
+  Serial.println(p_tm->tm_sec);
+  
+  // Clear the buffer.
+  display.clearDisplay();
+ 
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  
+  display.setCursor(0,0);
+  display.print(p_tm->tm_hour);
+  display.print(":");
+  if( p_tm->tm_min <10)
+    display.print("0"); 
+  display.print(p_tm->tm_min);
+  
+  display.setTextSize(2);
+  display.setCursor(90,5);
+  display.print(".");
+  if( p_tm->tm_sec <10)
+    display.print("0"); 
+  display.print(p_tm->tm_sec);
+
+  display.setTextSize(1);
+  display.setCursor(0,40);
+  display.print(p_tm->tm_mday);
+  display.print("/");
+  display.print(p_tm->tm_mon + 1);
+  display.print("/");
+  display.print(p_tm->tm_year + 1900);
+
+  display.display();
+
+  delay(1000); // update every 1 sec
+
+}
